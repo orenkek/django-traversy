44a06e3d9ec19b6ca8fb9c031b096ba87afb2f09
anuarcheck <anuarcheck@gmail.com>
2022-01-03 17:54:50 +0500

    python-django: CRUD

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<int:pk>', views.update_room, name='update-room'),
+    path('delete-room/<int:pk>', views.delete_room, name='delete-room')
 ]
